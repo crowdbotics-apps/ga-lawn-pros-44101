f704fe8b13dc92c9a75fc2daafa44cd930710b89
Crowdbotics <team@crowdbotics.com>
2023-10-23 00:38:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ga-lawn-pros-44101",
+  url: "https://ga-lawn-pros-44101.botics.co",
+  api: "https://ga-lawn-pros-44101.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
